06f4f94352c127fb5af8e11fd55fe922503f77ed
mfathur <mfathur17@gmail.com>
2020-02-26 05:59:24 +0700

    commit2

--- app/src/main/java/com/mfathurz/githubtraining/MainActivity.kt
+++ app/src/main/java/com/mfathurz/githubtraining/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Hello world
+        //I'll try my base
+        //asdasdasdasd
     }
 }
